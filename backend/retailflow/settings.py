6d393c55badaef353d0ccb064c177c05f0c0b4ed
Sundar_Reddy <balu112275@gmail.com>
2026-07-02 10:28:02 +0530

    Fix typo: rename retailfow to retailflow

--- backend/retailflow/settings.py
+++ backend/retailflow/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'retailfow.urls'
+ROOT_URLCONF = 'retailflow.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'retailfow.wsgi.application'
+WSGI_APPLICATION = 'retailflow.wsgi.application'
 
 
 # Database
